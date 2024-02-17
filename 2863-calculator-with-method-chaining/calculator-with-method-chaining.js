74cb0d51ff21c7112a8dccadb79ca55295ba9d97
Sharad Jain <sharadjain3010@gmail.com>
2024-02-18 02:13:23 +0530

    Time: 31 ms (99.94%) | Memory: 48.6 MB (29.21%) - LeetSync

---
+++ 2863-calculator-with-method-chaining/calculator-with-method-chaining.js
@@ -0,0 +1,64 @@
+class Calculator {
+    num;
+    /** 
+     * @param {number} value
+     */
+    constructor(value) {
+        this.num = value; 
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value){
+        this.num += value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value){
+        this.num -= value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */  
+    multiply(value) {
+         this.num *= value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    divide(value) {
+        if(value === 0){
+            throw "Division by zero is not allowed";
+        }
+         this.num /= value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+       this.num = Math.pow(this.num, value);
+        return this;
+    }
+    
+    /** 
+     * @return {number}
+     */
+    getResult() {
+        return this.num;
+    }
+}
