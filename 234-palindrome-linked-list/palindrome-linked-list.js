43dde4f4c5fac92e7aaa23c051725e0ab28e52f8
Sharad Jain <sharadjain3010@gmail.com>
2024-03-15 22:13:15 +0530

    Time: 117 ms (77.93%) | Memory: 69.7 MB (82.02%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -9,35 +9,6 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-// var isPalindrome = function(head) {
-//     const node = head;
-//     if(head == null || head.next == null){
-//         return true;
-//     }
-//     let reversedListHead = reverseLinkedList(node);
-//     while(head && reversedListHead){
-//         if(head.val !== reversedListHead.val){
-//             return false;
-//         }
-//         head = head.next;
-//         reversedListHead = reversedListHead.next;
-//     }
-//     return true;
-// };
-
-// var reverseLinkedList = function(head){
-//     let prev = null;
-    
-//     while(head){
-//         let nextNode = head.next;
-//         head.next = prev;
-//         prev = head;
-//         head = nextNode;
-//     }
-//     return prev;
-// }
-
-
 var isPalindrome = function(head) {
     let slow = head;
     let fast = head;
