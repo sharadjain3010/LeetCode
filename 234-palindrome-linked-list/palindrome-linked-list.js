e56b34ebf7d1ddeebe22a94e545c3a24589d5ed7
Sharad Jain <sharadjain3010@gmail.com>
2024-03-15 14:16:31 +0530

    Time: 114 ms (81.64%) | Memory: 69.5 MB (83.86%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -39,15 +39,30 @@
 
 
 var isPalindrome = function(head) {
-     let slow = head, fast = head, prev, temp
-    while (fast && fast.next)
-        slow = slow.next, fast = fast.next.next
-    prev = slow, slow = slow.next, prev.next = null
-    while (slow)
-        temp = slow.next, slow.next = prev, prev = slow, slow = temp
-    fast = head, slow = prev
-    while (slow)
-        if (fast.val !== slow.val) return false
-        else fast = fast.next, slow = slow.next
-    return true
+    let slow = head;
+    let fast = head;
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    let prev = slow;
+    slow = slow.next;
+    prev.next = null;
+    while(slow){
+       let temp = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = temp;
+    }
+
+    while(prev){
+        if(head.val !== prev.val){
+            return false;
+        }
+        head = head.next;
+        prev = prev.next;
+    }
+
+    return true;
 }
