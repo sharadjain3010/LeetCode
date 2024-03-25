0b0273469ea00ad1b42543c0d3eac3e4a090b425
Sharad Jain <sharadjain3010@gmail.com>
2024-03-25 23:52:29 +0530

    Time: 65 ms (25.54%) | Memory: 51.9 MB (10.32%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.js
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.js
@@ -10,31 +10,45 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var rightSideView = function(root) {
-    const queue = [];
-    const arr = [];
 
-    queue.push(root);
-
-    if(root === null){
-        return [];
+ var rightSideView = function(root, height = 1, arr = []) {
+    
+    if(root !== null){
+        if(arr.length < height ){
+            arr.push(root.val);
+        }
+        root.right && rightSideView(root.right, height+1, arr);
+        root.left && rightSideView(root.left, height+1, arr);
     }
-
-    while(queue.length > 0){
-        const qLen = queue.length;
-        const level = [];
-
-        for(let i=0; i<qLen; i++){
-            const el = queue.shift();
-            level.push(el.val);
-            el.left && queue.push(el.left);
-            el.right && queue.push(el.right);
-        }   
-
-        arr.push(level);
-    }
-
-    return arr.map((el)=>{
-        return el[el.length-1]
-    })
+    return arr;
 };
+
+ // traver for BFS and then take the right most node for right view. 
+// var rightSideView = function(root) {
+//     const queue = [];
+//     const arr = [];
+
+//     queue.push(root);
+
+//     if(root === null){
+//         return [];
+//     }
+
+//     while(queue.length > 0){
+//         const qLen = queue.length;
+//         const level = [];
+
+//         for(let i=0; i<qLen; i++){
+//             const el = queue.shift();
+//             level.push(el.val);
+//             el.left && queue.push(el.left);
+//             el.right && queue.push(el.right);
+//         }   
+
+//         arr.push(level);
+//     }
+
+//     return arr.map((el)=>{
+//         return el[el.length-1]
+//     })
+// };
