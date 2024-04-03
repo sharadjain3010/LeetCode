f4c12185abc825cc00555da31b243ec38374d96b
Sharad Jain <sharadjain3010@gmail.com>
2024-04-04 02:00:43 +0530

    Time: 70 ms (11.05%) | Memory: 52 MB (34.60%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    const arr = [];
+    const queue = [root];
+
+    if(root === null){
+        return 0;
+    }
+
+    while(queue.length>0){
+        const temp = [];
+        const queueLen = queue.length;
+        for(let i =0; i<queueLen; i++){
+            const el = queue.shift();
+            temp.push(el.val);
+            el.left && queue.push(el.left);
+            el.right && queue.push(el.right);
+        }
+
+        arr.push(temp);
+    }
+
+    return arr.length;
+};
