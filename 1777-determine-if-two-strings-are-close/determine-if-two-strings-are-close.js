6a7b94b3f26e347f7bb84710348951d941772a48
Sharad Jain <sharadjain3010@gmail.com>
2024-05-18 19:08:33 +0530

    Time: 164 ms (31.31%) | Memory: 57 MB (66.61%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {boolean}
+ */
+var closeStrings = function(word1, word2) {
+    if(word1.length !== word2.length){
+        return false;
+    }
+
+    let word1Map = {};
+    let word2Map = {};
+
+    for(let i=0; i<word1.length; i++){
+        word1Map[word1[i]] ? word1Map[word1[i]] += 1 : word1Map[word1[i]] = 1; 
+    }
+
+    for(let i=0; i<word2.length; i++){
+        word2Map[word2[i]] ? word2Map[word2[i]] += 1 : word2Map[word2[i]] = 1; 
+    }
+
+    return ((Object.keys(word1Map).sort().join('') === Object.keys(word2Map).sort().join(''))&&(Object.values(word1Map).sort((a,b)=>a-b).join('') === Object.values(word2Map).sort((a,b)=>a-b).join('')))
+
+};
