6df3f5ccaf6551b7a2cd8db87dd4bf23812b1052
Sharad Jain <sharadjain3010@gmail.com>
2024-05-27 08:17:00 +0530

    Time: 63 ms (41.59%) | Memory: 51.8 MB (49.11%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -11,31 +11,39 @@
  * @return {number}
  */
  // BFS solution
-var maxDepth = function(root) {
-    const arr = [];
-    const queue = [root];
+// var maxDepth = function(root) {
+//     const arr = [];
+//     const queue = [root];
 
+//     if(root === null){
+//         return 0;
+//     }
+
+//     while(queue.length>0){
+//         const temp = [];
+//         const queueLen = queue.length;
+//         for(let i =0; i<queueLen; i++){
+//             const el = queue.shift();
+//             temp.push(el.val);
+//             el.left && queue.push(el.left);
+//             el.right && queue.push(el.right);
+//         }
+
+//         arr.push(temp);
+//     }
+
+//     return arr.length;
+// };
+
+// DFS solution
+var maxDepth = function(root) {
     if(root === null){
         return 0;
     }
-
-    while(queue.length>0){
-        const temp = [];
-        const queueLen = queue.length;
-        for(let i =0; i<queueLen; i++){
-            const el = queue.shift();
-            temp.push(el.val);
-            el.left && queue.push(el.left);
-            el.right && queue.push(el.right);
-        }
-
-        arr.push(temp);
-    }
-
-    return arr.length;
-};
-
-// DFS solution
-// var maxDepth = function(root) {
    
-// };
+   const left = maxDepth(root.left);
+   const right = maxDepth(root.right);
+
+   return 1 + Math.max(left,right);
+
+};
