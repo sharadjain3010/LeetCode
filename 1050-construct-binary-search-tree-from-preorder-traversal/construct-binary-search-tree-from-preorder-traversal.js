3f02f9137b8570e6aad7a24dcd690ae8f85b07db
Sharad Jain <sharadjain3010@gmail.com>
2024-06-13 16:10:55 +0530

    Time: 71 ms (18.02%) | Memory: 52.2 MB (56.98%) - LeetSync

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @return {TreeNode}
+ */
+var bstFromPreorder = function(preorder) {
+    if(preorder.length == 0) return null;
+
+    const createNode = (preorder)=>{
+        // if(preorder.length == 0) return null;
+        let root = new TreeNode(preorder[0], null, null);
+        const leftTreeArr = preorder.filter((el)=>{return el<preorder[0]});
+        const rightTreeArr =  preorder.filter((el)=>{return el>preorder[0]});
+        root.left = (leftTreeArr.length === 0) ? null : createNode(leftTreeArr);
+        root.right = (rightTreeArr.length === 0) ? null : createNode(rightTreeArr);
+
+        return root;
+    }
+
+    return createNode(preorder);
+};
