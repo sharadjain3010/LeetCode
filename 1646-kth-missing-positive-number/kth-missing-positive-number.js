81f5daf963e2b736f9ebd461ad05be857a125a92
Sharad Jain <sharadjain3010@gmail.com>
2024-07-08 23:56:04 +0530

    Time: 53 ms (63.81%) | Memory: 50.1 MB (23.16%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.js
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.js
@@ -5,9 +5,20 @@
  */
 var findKthPositive = function(arr, k) {
     const seenArr = [];
-    for(let i=1; seenArr.length<k; i++){
-        if(!arr.includes(i)){
+    // for(let i=1; seenArr.length<k; i++){
+    //     if(!arr.includes(i)){
+    //         seenArr.push(i);
+    //     }
+    // }
+    let i =1;
+    let arrIndex = 0;
+    while(seenArr.length<k){
+        if(arr[arrIndex] === i){
+            i++;
+            arrIndex++;
+        }else{
             seenArr.push(i);
+            i++;
         }
     }
     return seenArr[seenArr.length-1];
