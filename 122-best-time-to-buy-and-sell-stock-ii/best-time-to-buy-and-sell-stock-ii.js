449581cb9e25e0238e95ed4a356ddd1c61394629
Sharad Jain <sharadjain3010@gmail.com>
2024-08-04 19:51:31 +0530

    Time: 53 ms (69.30%) | Memory: 49.7 MB (17.54%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -3,18 +3,49 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+    // let profit = 0;
+    // let buyIndex = 0;
+    // let sellIndex = 1;
+    // while(sellIndex < prices.length){
+    //     if(prices[buyIndex] < prices[sellIndex]){
+    //         profit += prices[sellIndex] - prices[buyIndex];
+    //         buyIndex++;
+    //         sellIndex++;
+    //     }else{
+    //         buyIndex = sellIndex;
+    //         sellIndex++;
+    //     }
+    // }
+    // return profit;
     let profit = 0;
     let buyIndex = 0;
-    let sellIndex = 1;
-    while(sellIndex < prices.length){
-        if(prices[buyIndex] < prices[sellIndex]){
-            profit += prices[sellIndex] - prices[buyIndex];
-            buyIndex++;
-            sellIndex++;
-        }else{
-            buyIndex = sellIndex;
-            sellIndex++;
+
+    for(let i=1; i<prices.length; i++){
+        let profitCal = prices[i] - prices[buyIndex]; 
+        if(profitCal > 0){
+            profit = profit + profitCal;
         }
+        buyIndex = i;
     }
     return profit;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
