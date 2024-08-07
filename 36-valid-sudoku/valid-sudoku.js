a3697b3cf7f366d0e3e329f4e0bf82b1d34db08b
Sharad Jain <sharadjain3010@gmail.com>
2024-08-07 23:12:08 +0530

    Time: 79 ms (22.53%) | Memory: 54.9 MB (14.87%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -3,19 +3,42 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
+    // let seen = {};
+    // for(let i=0; i<9; i++){
+    //     for(let j=0; j<9; j++){
+    //         let val = board[i][j];
+    //         if(val !== '.'){
+    //             if( seen[`${val}_is_seen_at_row_${i}`] ||
+    //             seen[`${val}_is_seen_at_column_${j}`] ||
+    //             seen[`${val}_is_seen_at_box_${Math.floor(i/3)}_${Math.floor(j/3)}`]){
+    //                 return false
+    //             }
+    //             seen[`${val}_is_seen_at_row_${i}`] = true;
+    //             seen[`${val}_is_seen_at_column_${j}`] = true;
+    //             seen[`${val}_is_seen_at_box_${Math.floor(i/3)}_${Math.floor(j/3)}`] = true;
+    //         }
+    //     }
+    // }
+    // return true;
+
+
+
+
+
+
     let seen = {};
     for(let i=0; i<9; i++){
         for(let j=0; j<9; j++){
-            let val = board[i][j];
-            if(val !== '.'){
-                if( seen[`${val}_is_seen_at_row_${i}`] ||
-                seen[`${val}_is_seen_at_column_${j}`] ||
-                seen[`${val}_is_seen_at_box_${Math.floor(i/3)}_${Math.floor(j/3)}`]){
-                    return false
+            let value = board[i][j];
+            if(value !== '.'){
+                if((seen[`${value}_seen_at_row_${i}`] ||
+                seen[`${value}_seen_at_column_${j}`] ||
+                seen[`${value}_seen_at_box_${Math.floor(i/3)}_${Math.floor(j/3)}`])){
+                    return false;
                 }
-                seen[`${val}_is_seen_at_row_${i}`] = true;
-                seen[`${val}_is_seen_at_column_${j}`] = true;
-                seen[`${val}_is_seen_at_box_${Math.floor(i/3)}_${Math.floor(j/3)}`] = true;
+                seen[`${value}_seen_at_row_${i}`] = true;
+                seen[`${value}_seen_at_column_${j}`] = true;
+                seen[`${value}_seen_at_box_${Math.floor(i/3)}_${Math.floor(j/3)}`] = true;
             }
         }
     }
