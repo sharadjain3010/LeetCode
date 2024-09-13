03607f649ce5f4a0387e4295779b1581e08ce064
Sharad Jain <sharadjain3010@gmail.com>
2024-09-13 23:00:56 +0530

    Time: 102 ms (35.11%) | Memory: 58.7 MB (57.69%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    let newIntervals = [];
+    let sortedIntervals = intervals.sort((a,b)=>{return a[0] - b[0]}); 
+     let currentInterval = sortedIntervals[0];
+    for(let i=1; i<sortedIntervals.length; i++){
+        if(currentInterval[0] > sortedIntervals[i][0]){
+            currentInterval[0] = sortedIntervals[i][0];
+        }
+        if(currentInterval[1] >= sortedIntervals[i][0] &&
+            currentInterval[1] < sortedIntervals[i][1]){
+            currentInterval[1] = sortedIntervals[i][1];
+        }
+        if(currentInterval[1] < sortedIntervals[i][0]){
+            newIntervals.push(currentInterval);
+            currentInterval = sortedIntervals[i];
+        }
+    
+    }
+    newIntervals.push(currentInterval);
+
+    return newIntervals;
+};
