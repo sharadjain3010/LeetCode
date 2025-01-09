0408ab11df417506ca4003081edb3cffd38c7fa8
Sharad Jain <sharadjain3010@gmail.com>
2025-01-09 14:55:27 +0530

    Time: 5 ms (97.15%) | Memory: 58.8 MB (72.95%) - LeetSync

---
+++ 326-power-of-three/power-of-three.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n === 1){
+        return true;
+    }
+
+    if(n<1 || n%1 !== 0){
+        return false;
+    }
+
+    return isPowerOfThree(n/3)
+};
