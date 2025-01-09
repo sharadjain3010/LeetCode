9a7f9f7bbade737212b9772d20e329699eaae5f8
Sharad Jain <sharadjain3010@gmail.com>
2025-01-09 15:44:40 +0530

    Time: 8 ms (35.63%) | Memory: 54.9 MB (19.54%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.js
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.js
@@ -4,21 +4,34 @@
  */
 var kthCharacter = function(k) {
     let word = 'a'
-    const generateWord = ()=>{
-        if(word.length >= k){
-            return;
-        }
+    // // Soultion 1 using recurssion
+    // const generateWord = ()=>{
+    //     if(word.length >= k){
+    //         return;
+    //     }
 
+    //     let wordArr = word.split('');
+    //     wordArr.forEach((char, i)=>{
+    //       wordArr.push(String.fromCharCode(word.charCodeAt(i)+1));
+    //     })
+
+    //     word = wordArr.join('');
+
+    //     generateWord(); 
+    // }
+
+    // generateWord();
+    // return word[k-1];
+
+    // // Soultion 2 using loop
+
+    while(word.length < k){
         let wordArr = word.split('');
         wordArr.forEach((char, i)=>{
           wordArr.push(String.fromCharCode(word.charCodeAt(i)+1));
         })
 
         word = wordArr.join('');
-
-        generateWord(); 
     }
-
-    generateWord();
     return word[k-1];
 };
