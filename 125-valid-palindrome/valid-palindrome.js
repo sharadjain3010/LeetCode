e11554bc34e8e55421a20f1347b4e8c4b73bfc8b
Sharad Jain <sharadjain3010@gmail.com>
2025-01-15 23:27:09 +0530

    Time: 4 ms (73.76%) | Memory: 51.1 MB (87.55%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    const updatedStr = s.replace(/[^a-z0-9]/gmi,'');
+    const updatedStr = s.replace(/[^a-z0-9]/gmi,'').toLowerCase();
 
     if(updatedStr.length <= 1){
         return true;
@@ -13,7 +13,7 @@ var isPalindrome = function(s) {
     let i = 0;
     let j = updatedStr.length-1; 
     while(i<j){
-        if(updatedStr[i].toLowerCase() !== updatedStr[j].toLowerCase()){
+        if(updatedStr[i] !== updatedStr[j]){
             isPalindrome = false;
             break;
         }
